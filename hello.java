d07f460611e36ab7cbd5a4b42a855edd211f3f26
Rupeshsahane89 <154776703+Rupeshsahane89@users.noreply.github.com>
2024-01-24 16:24:52 +0530

    initial commit

--- hello.java
+++ hello.java
@@ -6,7 +6,7 @@ public class hello
         System.out.println(new Date());
         System.out.println("Hello world");
         System.out.println("********");
-        System.out.println("added one more filevsg");
+        System.out.println("added one more filevsg"
         student stud = new student();
         stud.sayHello();
         System.out.println("AWS program run sucessfully\n");
